9f96020e4e0e8c7f370b306671a42d50b11a44cd
setthasak11 <120776787+setthasak11@users.noreply.github.com>
2024-03-26 02:15:58 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,14 +3,14 @@ var InitialCount = -1;
 
 
 const deleteProducts = async() => {
-    url = 'https://energetic-puce-walrus.cyclic.app/product';
+    url = 'https://databasests.netlify.app/product';
 
     let res = await axios.get(url);
     responseText = res.data;
     const products = responseText;
 
     for (let product of products) {
-        const response = await axios.delete(`https://energetic-puce-walrus.cyclic.app/product/${product.id}`)
+        const response = await axios.delete(`https://databasests.netlify.app/product/${product.id}`)
 
     }
     location.reload();
@@ -22,7 +22,7 @@ const deleteProducts = async() => {
 }
 
 const loadProducts = async() => {
-    url = 'https://energetic-puce-walrus.cyclic.app/product';
+    url = 'https://databasests.netlify.app/product';
 
     let res = await axios.get(url);
     responseText = await res.data;
@@ -79,7 +79,7 @@ const loadProducts = async() => {
 var checkout = async() => {
     document.getElementById('2').innerHTML = "<span class='loader-16' style='margin-left: 44%;'></span>"
     var payable = 0;
-    url = 'https://energetic-puce-walrus.cyclic.app/product';
+    url = 'https://databasests.netlify.app/product';
 
     let res = await axios.get(url);
     responseText = await res.data;
